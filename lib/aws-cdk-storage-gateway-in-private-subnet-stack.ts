ba403c234fca10bbbdf96c44c1ed61fce5267cca
nakajima97 <kituneudon97@gmail.com>
2024-04-04 08:11:45 +0900

    セッションマネージャで接続できるように雑にパブリックサブネットに配置

--- lib/aws-cdk-storage-gateway-in-private-subnet-stack.ts
+++ lib/aws-cdk-storage-gateway-in-private-subnet-stack.ts
@@ -14,14 +14,22 @@ export class AwsCdkStorageGatewayInPrivateSubnetStack extends cdk.Stack {
     // private subnetを取得
     const privateSubnets = vpc.privateSubnets;
 
+    // IAMロールを作成
+    const instanceRole = new cdk.aws_iam.Role(this, 'InstanceRole', {
+      assumedBy: new cdk.aws_iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+
+    instanceRole.addManagedPolicy(cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
+
     // pirvate subnetにEC2インスタンスを作成
     const ec2 = new cdk.aws_ec2.Instance(this, 'Instance', {
       vpc,
       instanceType: new cdk.aws_ec2.InstanceType('t2.micro'),
       machineImage: new cdk.aws_ec2.AmazonLinuxImage(),
       vpcSubnets: {
-        subnets: privateSubnets,
+        subnets: vpc.publicSubnets,
       },
+      role: instanceRole
     });
 
     // private subnetにStorage Gateway用のEC2インスタンスを作成
