d5ea4d88ca9556ed02c6b0b00df65e9551124c70
nakajima97 <kituneudon97@gmail.com>
2024-04-03 21:46:13 +0900

    検証に使う資材を配置

--- lib/aws-cdk-storage-gateway-in-private-subnet-stack.ts
+++ lib/aws-cdk-storage-gateway-in-private-subnet-stack.ts
@@ -6,11 +6,32 @@ export class AwsCdkStorageGatewayInPrivateSubnetStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPCを作成
+    const vpc = new cdk.aws_ec2.Vpc(this, 'VPC', {
+      maxAzs: 2,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkStorageGatewayInPrivateSubnetQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // private subnetを取得
+    const privateSubnets = vpc.privateSubnets;
+
+    // pirvate subnetにEC2インスタンスを作成
+    const ec2 = new cdk.aws_ec2.Instance(this, 'Instance', {
+      vpc,
+      instanceType: new cdk.aws_ec2.InstanceType('t2.micro'),
+      machineImage: new cdk.aws_ec2.AmazonLinuxImage(),
+      vpcSubnets: {
+        subnets: privateSubnets,
+      },
+    });
+
+    // private subnetにStorage Gateway用のEC2インスタンスを作成
+    const storageGatewayEc2 = new cdk.aws_ec2.Instance(this, 'StorageGatewayInstance', {
+      vpc,
+      instanceType: new cdk.aws_ec2.InstanceType('i4i.large'),
+      machineImage: new cdk.aws_ec2.AmazonLinuxImage(),
+      vpcSubnets: {
+        subnets: privateSubnets,
+      },
+    });
   }
 }
